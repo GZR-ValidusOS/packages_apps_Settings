3f30727c579acbb3db183a1312e7f90d42273ef2
Martinusbe <martinusbe@gmail.com>
2017-01-11 16:49:28 +0100

    dashboard suggestions default disabled
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- src/com/android/settings/dashboard/DashboardAdapter.java
+++ src/com/android/settings/dashboard/DashboardAdapter.java
@@ -115,7 +115,7 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
 
     public List<Tile> getSuggestions() {
         if ((Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
+                    Settings.System.DISABLE_SUGGESTIONS, 0) == 1)) {
              return mSuggestions;
         } else {
              return null;
@@ -125,7 +125,7 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
     public void setCategoriesAndSuggestions(List<DashboardCategory> categories,
             List<Tile> suggestions) {
         if ((Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
+                    Settings.System.DISABLE_SUGGESTIONS, 0) == 1)) {
              mSuggestions = suggestions;
         } else {
              mSuggestions = null;
