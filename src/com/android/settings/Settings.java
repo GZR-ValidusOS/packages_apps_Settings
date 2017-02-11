ff5684102584b056073d478ef6ef8f677dea1fcd
nathanchance <natechancellor@gmail.com>
2017-02-11 13:36:58 -0500

    Add Magisk Manager dashboard option
    The primary reason for bringing this into the dashboard is that the
    Magisk Manager will soon have root management options, meaning that
    people will no longer need the Superuser app
    
    I would have liked to use the actual Magisk icon from within the app
    but it just does not look good at lower DPIs so I picked a tuning icon
    from https://materialdesignicons.com/
    
    Preview with summary: http://i.imgur.com/Zc1FJN8.png
    
    Change-Id: If7df9976d371b29797ec4c237c77c7489f6a3541
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    GZR: Made a vector to match magiskmanager arranged custom dash setting a lil bit

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -166,4 +166,5 @@ public class Settings extends SettingsActivity {
     public static class SmartbarSettingsActivity extends SettingsActivity { /* empty */ }
     public static class PulseSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
+    public static class MagiskActivity extends SettingsActivity { /* empty */ }
 }
--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -235,6 +235,8 @@ public class SettingsActivity extends SettingsDrawerActivity
 
     private static final int REQUEST_SUGGESTION = 42;
 
+    private static final String MAGISK_FRAGMENT = "com.android.settings.MagiskManager";
+
     private String mFragmentClass;
 
     private CharSequence mInitialTitle;
@@ -1030,6 +1032,13 @@ public class SettingsActivity extends SettingsDrawerActivity
      */
     private Fragment switchToFragment(String fragmentName, Bundle args, boolean validate,
             boolean addToBackStack, int titleResId, CharSequence title, boolean withTransition) {
+        if (MAGISK_FRAGMENT.equals(fragmentName)) {
+            Intent magiskIntent = new Intent();
+            magiskIntent.setClassName("com.topjohnwu.magisk", "com.topjohnwu.magisk.SplashActivity");
+            startActivity(magiskIntent);
+            finish();
+            return null;
+        }
         if (validate && !isValidFragment(fragmentName)) {
             throw new IllegalArgumentException("Invalid fragment for this activity: "
                     + fragmentName);
@@ -1122,6 +1131,16 @@ public class SettingsActivity extends SettingsDrawerActivity
 
         // Reveal development-only quick settings tiles
         DevelopmentTiles.setTilesEnabled(this, showDev);
+
+        // Magisk Manager
+        boolean magiskSupported = false;
+        try {
+            magiskSupported = (getPackageManager().getPackageInfo("com.topjohnwu.magisk", 0).versionCode > 0);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        setTileEnabled(new ComponentName(packageName,
+                        Settings.MagiskActivity.class.getName()),
+                magiskSupported, isAdmin, pm);
 
         if (UserHandle.MU_ENABLED && !isAdmin) {
             // When on restricted users, disable all extra categories (but only the settings ones).
